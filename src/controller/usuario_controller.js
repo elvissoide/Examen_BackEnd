825fbd024db12157e00031ebdccb1e3a936f493e
Elvis Guanoluisa <elviserock@gmail.com>
2023-10-02 00:44:43 -0500

    Login usuario

--- src/controller/usuario_controller.js
+++ src/controller/usuario_controller.js
@@ -1,6 +1,35 @@
 import Usuario from "../models/Usuario.js";
 import mongoose from "mongoose";
 
+function crearToken () {
+    const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let token = '';
+    for (let i = 0; i < 20; i++) {
+        const indiceAleatorio = Math.floor(Math.random() * caracteres.length);
+        token += caracteres.charAt(indiceAleatorio);
+    }
+    console.log(token);
+    return token;
+}
+
+const login = async(req,res)=>{
+    const {email,password} = req.body
+    if (Object.values(req.body).includes("")) return res.status(404).json({msg:"Lo sentimos, debes llenar todos los campos"})
+    const usuarioBDD = await Usuario.findOne({email}).select("-__v -token -updatedAt -createdAt")
+    if(!usuarioBDD) return res.status(404).json({msg:"Lo sentimos, el usuario no se encuentra registrado"})
+    const verificarPassword = await usuarioBDD.matchPassword(password)
+    if(!verificarPassword) return res.status(404).json({msg:"Lo sentimos, el password no es el correcto"})
+    const token = crearToken();
+    const {nombre,apellido,_id} = usuarioBDD
+    res.status(200).json({
+        token,
+        nombre,
+        apellido,
+        _id,
+        email:usuarioBDD.email
+    })
+}
+
 const registro = async (req, res) => {
     const { email, password } = req.body
     if (Object.values(req.body).includes("")) return res.status(400).json({ msg: "Lo sentimos, debes llenar todos los campos" })
@@ -11,8 +40,9 @@ const registro = async (req, res) => {
     res.status(200).json({ msg: "Exito. Cuenta registrada exitosamente." })
 }
 
-// nuevoUsuario.crearToken()
+// 
 
 export {
+    login,
     registro
 }
--- src/router/usuario_routes.js
+++ src/router/usuario_routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import {
+    login,
     registro
 } from "../controller/usuario_controller.js";
 
 const router = Router();
 
+router.post('/login',login)
 router.post('/registro',registro)
 
 export default router;
